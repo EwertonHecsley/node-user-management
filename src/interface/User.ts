b452bfcaaa49e62d4803fc3ac75da6b5b4da9c66
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-02-08 20:55:04 -0300

    criando inteface de usuario

---
+++ src/interface/User.ts
@@ -0,0 +1,11 @@
+export interface IUSer {
+    id?: number;
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+    created_at?: Date;
+    updated_at?: Date;
+
+
+}
