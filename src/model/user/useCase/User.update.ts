ab61dd29318ed03469ef377b952a6b34121971bb
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-02-10 14:37:49 -0300

    update user refatorando

--- src/model/user/useCase/User.update.ts
+++ src/model/user/useCase/User.update.ts
@@ -1,3 +1,4 @@
+import { number } from "zod";
 import { DatabaseService } from "../../../database/Database.service";
 import { IUSer } from "../../../interface/User";
 import { HttpException } from "../../../middleware/HttpException";
@@ -16,19 +17,30 @@ export class UserUpdateService {
     async updateUSer(): Promise<IUSer[]> {
         const { id, name, username, email, password } = this.user;
 
-        if (!id) throw new HttpException(404, 'Usuário não encontrado.');
+        if (id === undefined) {
+            throw new HttpException(400, 'ID do usuário não fornecido.');
+        }
+
+        const userIdExist = await this.userDatabaseService.getUserById(id);
+
+        if (!userIdExist) {
+            throw new HttpException(404, 'Usuário não encontrado.');
+        }
 
         const usernameExist = await this.userDatabaseService.getUSerByUsername(username);
-        if (usernameExist) throw new HttpException(400, 'Username já existe.')
+        if (usernameExist) {
+            throw new HttpException(400, 'Username já existe.');
+        }
 
         const emailExist = await this.userDatabaseService.getUserByEmail(email);
-        if (emailExist) throw new HttpException(400, 'Email já existe.')
+        if (emailExist) {
+            throw new HttpException(400, 'Email já existe.');
+        }
 
         const hashedPassword = await this.userServiceHash.hashPassword(password);
 
         const user = new UserUpdateModel({ name, username, email, password: hashedPassword });
 
         return await user.updateUser(id);
-
     }
 }
