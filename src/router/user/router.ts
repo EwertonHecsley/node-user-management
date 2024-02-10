3c92abafbcebbf7862f35758f22b3f6a4e52be3a
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-02-10 12:32:10 -0300

    metodos de busca de usuarios na rota

--- src/router/user/router.ts
+++ src/router/user/router.ts
@@ -11,6 +11,8 @@ router.get('/', (req, res) => {
 
 router.post('/user', router_user.createUser);
 router.get('/user/:id', router_user.getUserById);
+router.get('/user/:username', router_user.getUserByUsername);
+router.get('/user/:email', router_user.getUserByEmail);
 router.get('/user', router_user.getUsers);
 
 
