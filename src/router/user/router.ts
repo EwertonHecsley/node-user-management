c5be26ef5dfcf81a2a48a2f11ebaacef17627add
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-02-10 14:14:22 -0300

    update user adicionando na rota

--- src/router/user/router.ts
+++ src/router/user/router.ts
@@ -5,15 +5,13 @@ const router = Router();
 
 const router_user = new UserController();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
 router.post('/user', router_user.createUser);
+
 router.get('/user/username/:username', router_user.getUserByUsername);
 router.get('/user/email/:email', router_user.getUserByEmail);
 router.get('/user/:id', router_user.getUserById);
 router.get('/user', router_user.getUsers);
 
+router.put('/user', router_user.updateUser);
 
 export default router;
