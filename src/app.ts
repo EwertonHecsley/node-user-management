053ceefd2d387960f44630055df9c0a357720887
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-02-10 11:48:08 -0300

    adicionando cors e express error

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
 import router from './router/user/router';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
