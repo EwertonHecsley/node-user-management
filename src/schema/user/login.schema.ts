08ce032572364e52db059b023c595a2875761ff4
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-02-11 15:28:37 -0300

    ajuste na mensagem

--- src/schema/user/login.schema.ts
+++ src/schema/user/login.schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const loginSchema = z.object({
     usernameOrEmail: z.string({
-        required_error: 'Campo username é obrigatório.'
+        required_error: 'Campo username/email é obrigatório.'
     }),
     password: z.string({
         required_error: 'Campo password é obrigatório.'
